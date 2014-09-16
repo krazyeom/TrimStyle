3590cedf02ae4d42c13123ab5d0e4b6e861ab634
Steve Yeom <krazyeom@gmail.com>
2014-09-17 07:58:41 +0900

    Initial Commit

---
+++ TrimStyle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TrimStyle
+//
+//  Created by Steve Yeom on 9/17/14.
+//  Copyright (c) 2014 2nd Jobs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TrimStyle/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  TrimStyle
+//
+//  Created by Steve Yeom on 9/17/14.
+//  Copyright (c) 2014 2nd Jobs. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}
